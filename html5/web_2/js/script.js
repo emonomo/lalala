d71d1cc9f5f2166ef3706eeb140911f536987989
machi <joanna881110@gmail.com>
2023-05-16 17:04:14 +0800

    0516

--- html5/web_2/js/script.js
+++ html5/web_2/js/script.js
@@ -10,3 +10,21 @@ function bannerSwitcher() {
     clearInterval(bannerTimer);
     bannerTimer = setInterval(bannerSwitcher, 5000)
   });
+
+
+
+ $(document).ready(function () {
+  //---漢堡按鈕---
+    $('.hamburger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.navigation').toggleClass('show');
+    }); 
+
+    //---按鈕按下就跳轉指定的區塊---
+    $(".menu a").click(function(){
+      var btn = $(this).attr("href");//return取得屬性與值
+      var pos = $(btn).offset();//抓取相對座標位置
+      $("html,body").animate({scrollTop:pos.top},1500);//最後面的(1000是一秒)(1500為1.5秒)
+    });
+});
+
