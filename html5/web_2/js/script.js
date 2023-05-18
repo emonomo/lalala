312e1246d265b97eafc88ed261b8b54d3c83db8b
machi <joanna881110@gmail.com>
2023-05-18 17:01:27 +0800

    0518

--- html5/web_2/js/script.js
+++ html5/web_2/js/script.js
@@ -20,11 +20,29 @@ function bannerSwitcher() {
         $('.navigation').toggleClass('show');
     }); 
 
-    //---按鈕按下就跳轉指定的區塊---
+   //---按鈕按下就跳轉指定的區塊---
     $(".menu a").click(function(){
       var btn = $(this).attr("href");//return取得屬性與值
       var pos = $(btn).offset();//抓取相對座標位置
       $("html,body").animate({scrollTop:pos.top},1500);//最後面的(1000是一秒)(1500為1.5秒)
+    });
+
+    //滑動置頂
+    $('#gotop').click(function(){
+      $('html,body').animate({scrollTop:0},1500);
+    })
+    //置頂按鈕淡出淡入
+    $(window).scroll(function(){
+      if($(this).scrollTop()>200){
+        $('#gotop').stop().fadeTo('fast',1);   //.fadeTo(1000,1) => 1000是一秒，沒有給速度的話要給空值 ""(預設為0.4秒)
+      }else{
+        $('#gotop').stop().fadeOut();
+      }
+    })
+
+    //---滑動載入---
+    $('.smoove').smoove({
+      offset: '30%'
     });
 });
 
